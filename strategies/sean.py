15930ecc058507411ab205d9919ca1fbaaaef3d8
Stefan Ciutina <iJynx@protonmail.com>
2025-08-15 02:27:34 +0800

    made the algorithm better

--- strategies/sean.py
+++ strategies/sean.py
@@ -1,3 +1,14 @@
+def buy():
+    print ("buy")
+def sell():
+    print ("sell")
+price = 50000
+
 def sean():
     # very novel trading strategy
     print("Buy high, sell low")
+    
+    if price > 100:
+        buy()
+    if price < 10:
+        sell()
